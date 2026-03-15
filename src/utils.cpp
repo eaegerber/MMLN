adc871b756af93aca0ce3ec56f4cbce620297cff
Jack Dreifus <j.dreifus@northeastern.edu>
2026-03-15 11:35:47 -0400

    add utiliy file with mock functions

---
+++ src/utils.cpp
@@ -0,0 +1,35 @@
+#include <RcppArmadillo.h>
+#include <RcppEigen.h>
+
+using namespace Rcpp; // RcppArmadillo.h includes Rcpp.h interally
+using namespace arma;
+
+// Aliases for common matrix/vector types
+typedef arma::mat Mat;
+typedef arma::vec Vec;
+typedef arma::rowvec RowVec;
+typedef arma::uvec UVec;
+
+// Some mock utility functions, may be removed in the future or could be used.
+
+// Clamp each element of a vector to [lo, hi], using RcppArmadillo
+// [[Rcpp::export]]
+arma::vec clamp_vec(arma::vec x, double lo, double hi)
+{
+  return arma::clamp(x, lo, hi);
+}
+
+// Compute column-wise means of a matrix, using RcppArmadillo
+// [[Rcpp::export]]
+arma::rowvec col_means(arma::mat X)
+{
+  return arma::mean(X, 0);
+}
+
+// Compute eigenvalues of a symmetric matrix, using RcppEigen
+// [[Rcpp::export]]
+Eigen::VectorXd sym_eigenvalues(Eigen::MatrixXd X)
+{
+  Eigen::SelfAdjointEigenSolver<Eigen::MatrixXd> solver(X);
+  return solver.eigenvalues();
+}
